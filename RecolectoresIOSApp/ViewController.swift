d3a8f69ea48a902bb3d2d74ac8401a2d3c3a4db1
David Martinez <a01735425@tec.mx>
2023-11-01 15:14:20 -0600

    pantalla login bien en diseño

--- RecolectoresIOSApp/ViewController.swift
+++ RecolectoresIOSApp/ViewController.swift
@@ -9,22 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
     @IBOutlet weak var userTextField: UITextField!
-    
     @IBOutlet weak var passwordTextField: UITextField!
-    
-    
-
-
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        userTextField.clipsToBounds = true
+        userTextField.layer.cornerRadius = 10.0
+        userTextField.layer.borderWidth = 1.0
+        userTextField.layer.borderColor = UIColor.red.cgColor
+        
         passwordTextField.clipsToBounds = true
         passwordTextField.layer.cornerRadius = 10.0
-        passwordTextField.layer.borderWidth = 1.0  
+        passwordTextField.layer.borderWidth = 1.0
         passwordTextField.layer.borderColor = UIColor.red.cgColor
     }
 
