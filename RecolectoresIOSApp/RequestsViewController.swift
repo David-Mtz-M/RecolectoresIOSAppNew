ba52d989f5d3450e08fd6e5e8261b12831a69c6c
David Martinez <a01735425@tec.mx>
2023-11-07 17:47:44 -0600

    jalo

--- RecolectoresIOSApp/RequestsViewController.swift
+++ RecolectoresIOSApp/RequestsViewController.swift
@@ -28,8 +28,17 @@ class RequestsViewController: UIViewController {
         
         var profileImg = UIImage(named: "profile")
         profileImg = profileImg?.withRenderingMode(.alwaysOriginal)
+        
+        var returnImg = UIImage(named: "returnIcon")
+        returnImg = returnImg?.withRenderingMode(.alwaysOriginal)
     
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: profileImg, style: .done, target: self, action: nil)
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: returnImg, style: .done, target: self, action: #selector(moveBackToOptions))
+    }
+    
+    @objc private func moveBackToOptions() {
+        let mainViewController = self.storyboard?.instantiateViewController(withIdentifier: "OptionsStoryboard") as! OpcionesViewController
+        self.navigationController?.pushViewController(mainViewController, animated: true)
     }
     
     
