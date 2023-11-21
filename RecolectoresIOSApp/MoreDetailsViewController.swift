f3d0515736fc08fbe8ee55e3b4a0989425f2d6e0
Fernando Estrada <fernaest213@gmail.com>
2023-11-21 15:52:15 -0600

    Integración de audio en el botón de MoreDetails(No se oye en la Mac)

--- RecolectoresIOSApp/MoreDetailsViewController.swift
+++ RecolectoresIOSApp/MoreDetailsViewController.swift
@@ -11,13 +11,22 @@ import AVFoundation
 class MoreDetailsViewController: UIViewController {
     
     var audioPlayer: AVAudioPlayer?
-    let audioFiles = ["audio1"]
+    let audioFiles = ["aceiteAuto", "aceiteUsado", "arbol", "baterias", "bici", "botellas", "carton", "electronicos", "escombro", "industriales", "juguetes", "libros", "llantas", "madera", "medicina", "metal", "organico", "pallets", "papel", "pilas", "plasticos", "ropa", "tapas", "tetrapack", "toner", "voluminoso"]
     
-    @IBAction func playSoundButton(_ sender: Any) {
+    @IBAction func playSoundButton(_ sender: UIButton) {
+        let index = sender.tag
+        guard index >= 0, index < audioFiles.count else{
+            print("Índice de botón no válido.")
+            return
+        }
         
+        let fileName = audioFiles[index]
+        
+        playAudio(filename: fileName)
     }
     @IBOutlet weak var lbl_Description: UILabel!
     @IBOutlet weak var img_View: UIImageView!
+    @IBOutlet weak var playSoundButton: UIButton!
     var img = UIImage()
     var des = ""
     
@@ -26,6 +35,20 @@ class MoreDetailsViewController: UIViewController {
 
         lbl_Description.text = des
         img_View.image = img 
+    }
+    
+    func playAudio(filename: String) {
+        if let path = Bundle.main.path(forResource: filename, ofType: "mp3") {
+            let url = URL(fileURLWithPath: path)
+            
+            do {
+                audioPlayer?.stop()
+                audioPlayer = try AVAudioPlayer(contentsOf: url)
+                audioPlayer?.play()
+            } catch {
+                print("Error al inicializar AVAudioPlayer: \(error.localizedDescription)")
+            }
+        }
     }
     
 
