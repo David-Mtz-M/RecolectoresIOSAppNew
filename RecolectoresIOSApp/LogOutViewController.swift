0d0df6b5a21a34817c5cc2ae17a392c708c360dd
David Martinez <a01735425@tec.mx>
2023-11-24 20:09:14 -0600

    Pop up para el cierre de sesión (No hace alguna acción)

--- RecolectoresIOSApp/LogOutViewController.swift
+++ RecolectoresIOSApp/LogOutViewController.swift
@@ -9,6 +9,32 @@ import UIKit
 
 class LogOutViewController: UIViewController {
     
+    @IBOutlet weak var MailField: UITextField!
+    @IBOutlet weak var secondLogOut: UIButton!
+    
+    
+    //Se realiza un pop up para avisar del cierre de sesión
+    
+    @IBAction func popUpTagged(_ sender: UIButton) {
+        let alertController = UIAlertController(title: "¿Seguro?", message: "Esta acción es irreversible", preferredStyle: .alert)
+        //Botón que hace que el usuario cierre sesión
+        let acceptAction = UIAlertAction(title: "Salir", style: .default) {_ in
+        print("Botón Aceptar Presionado")
+        }
+        alertController.addAction(acceptAction)
+        
+        //Botón que hace que se cancele el cierre de sesión
+        let closeAction = UIAlertAction(title: "Cancelar", style: .cancel) {_ in
+            print("Pop up cerrado")
+        }
+        alertController.addAction(closeAction)
+        
+        //Hace en negrita la acción principal
+        alertController.preferredAction = acceptAction
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    
 }
 
 
--- RecolectoresIOSApp/ProfileViewController.swift
+++ RecolectoresIOSApp/ProfileViewController.swift
@@ -82,14 +82,6 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
         present(imagePicker, animated: true, completion: nil)
     }
     
-    @IBAction func cerrarSesionTapped(_ sender: UIButton) {
-        performSegue(withIdentifier: "LogOutStoryBoard", sender: self)
-    }
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "LogOutStoryBoard"{
-            
-        }
-    }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let pickedImage = info[.originalImage] as? UIImage {
